c72d2bd24495e75e2cc781eb25b70b4104c462c4
sheygs13 <segun.ekoh@gmail.com>
2020-05-10 16:08:03 +0100

    :fire:

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 //Global App Controller
-import number from './test';
-const name = 'Olusegun';
-console.log(`I imported ${number} from test.js by ${name}`);
+import Search from './models/Search';
+import { handleErrors } from '../../handlers/handlers';
+
+const search = new Search('rice');
+const result = search.getRecipe();
+result.catch(handleErrors);
+
+
