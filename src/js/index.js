4c55074ff23718eeab8c94711c65de3854bdc100
sheygs13 <segun.ekoh@gmail.com>
2020-05-11 14:33:43 +0100

    feat(controller-logic): implement search controller logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,48 @@
-//Global App Controller
 import Search from './models/Search';
 import { handleErrors } from '../../handlers/handlers';
+import { element } from './views/base';
+import * as searchView from './views/searchView';
 
-const search = new Search('rice');
-const result = search.getRecipe();
-result.catch(handleErrors);
+/**
+ * Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping List
+ * - Liked recipes
+ */ 
+const state = {};
+
+
+ async function handleSubmit(e) {
+
+  e.preventDefault();
+
+  // input field value
+  const query = searchView.getInput();
+  
+  // only proceed if there is a value
+  if (query){
+     // search object
+     state.search = new Search(query);
+
+     // disable button to prevent double
+     // form submission
+     element.searchButton.disabled = true;
+
+     // prepare UI for changes
+
+     // search for recipes 
+     await state.search.getRecipe().catch(handleErrors);
+
+     element.searchButton.disabled = false;
+     console.log(await state.search.getRecipe());
+
+     // display result on UI
+  }
+  
+}
+element.searchForm.addEventListener('submit', handleSubmit);
+
+
 
 
