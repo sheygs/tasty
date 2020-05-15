b0e80e5cfd2d3cf234085e008c996c2314f2f2ed
sheygs13 <segun.ekoh@gmail.com>
2020-05-15 12:21:29 +0100

    rm -rf :fire:

--- handlers/handlers.js
+++
@@ -1,7 +0,0 @@
-export function handleErrors({ message }){
- const statusCode = message.slice(-3);
- const text = message.slice(0,14)
- console.info('Something went wrong 😕')
- console.log(`${statusCode}: ${text}`);
- alert(`${statusCode}: ${text}`)
-}
