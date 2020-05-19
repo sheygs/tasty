a479be4106bd1af9a6472ec650d29ab034a8a849
sheygs13 <segun.ekoh@gmail.com>
2020-05-19 22:43:07 +0100

    implement recipe model

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -12,7 +12,7 @@ class Recipe {
    }
 
 
-   async getRecipe(){
+   async getRecipe() {
      try {
 
       const result = await axios.get(`${config.cors}${config.baseEndPoint}/get?rId=${this.id}`);
@@ -29,17 +29,40 @@ class Recipe {
      }
    }
 
-   calculateTime(){
+   calculateTime() {
+
     // assume we have 15 mins for every 3 ingredients
     const totalIngredients = this.ingredients.length;
     const period = Math.ceil(totalIngredients / 3);
     this.time = period * 20;
+    
    }
 
    calculateServings(){
      this.servings = 4;
    }
    
+
+   parseIngredients(){
+     const newIngredients = this.ingredients.map(el => {
+
+          const longUnits = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+          const shortUnits = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+          // 1) Uniform Ingredients
+          let ingredient = el.toLowerCase();
+          longUnits.forEach((unit, index) => {
+            ingredient = ingredient.replace(unit, shortUnits[index]);   
+          }); 
+
+           // 2) Remove parenthesis
+           ingredient = ingredient.replace(/ *\([^)]*\) */g, "");
+
+          // 3) Parse ingredients into count, unit and ingredient
+
+     });
+     this.ingredients = newIngredients;
+   }
 }
 
 export default Recipe;
